a228bb81ef7b511633a2051ad60b12c4f9c39782
Georges Emmanuel Diene NDONG <georgesemmanuel96@gmail.com>
2018-02-17 12:06:47 +0000

    commit

--- formulaire/formulaire.php
+++ formulaire/formulaire.php
@@ -18,7 +18,7 @@
         <legend> Vos Informations</legend>
         <p><label for="nom" >Nom: </label><input type"text" name="nom" id="nom" style="..."/><p></br>
         <p><label for="prenom"> Prenom : </label><input type"text" name="prenom" id="prenom" style="..."/><p></br>
-        <p><label for="adresse"> Adresse : </label><input type"text" name="ville" id="ville" style="..."/><p></br>
+        <p><label for="adresse"> Adresse : </label><input type"text" name="adresse" id="adresse" style="..."/><p></br>
         <p><label for="ville"> Ville : </label><input type"text" name="ville" id="ville" style="..."/><p></br>
         <p><label for="code"> Code : </label><input type"text" name="code" id="code" style="..."/><p></br>
 
